baba25240b6d19f86ac5d2e7db7270ca312bf3dd
lucjalbert <lucjalbert01@gmail.com>
2024-11-03 22:05:19 -0500

    Add textnode eq unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("Text here", TextType.LINK, "here is a url")
+        node2 = TextNode("Text here", TextType.LINK, "here is a url")
+        self.assertEqual(node, node2)
+    
+    def test_eq_failure(self):
+        node = TextNode("Text here", TextType.LINK, "here is a url")
+        node2 = TextNode("different text here", TextType.LINK, "here is a url")
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
